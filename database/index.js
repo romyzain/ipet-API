ab0428023c189c4f986a744593b4b849e5684a2c
romyzain <romisadad@gmail.com>
2020-04-25 15:05:10 +0700

    first commit

---
+++ database/index.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql')
+const util = require('util')
+
+const db = mysql.createConnection({
+    host : 'localhost',
+    user : 'root',
+    password : 'bi5miLLah',
+    database : 'ipet',
+    port : 3306
+})
+
+const query = util.promisify(db.query).bind(db)
+module.exports = {
+    db,
+    query
+}
